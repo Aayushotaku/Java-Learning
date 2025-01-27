869b02a5234f269fdb48ef7fadf5c87d49352b50
Aayush Bajracharya <aayushbajracharya65@gmail.com>
2025-01-27 11:19:09 +0545

    Push

--- Datatypes.java
+++ Datatypes.java
@@ -1,11 +1,12 @@
 import java.util.Arrays;// Package
 
 public class Datatypes {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
         // Variables: Variables are like containers where values can be stored.
         // Datatypes: The type of data that variables can hold.
-        // Java is a statically typed language, which means every variable must have a specific datatype before use.
-        
+        // Java is a statically typed language, which means every variable must have a
+        // specific datatype before use.
+
         // Types of data types:
         // 1. Primitive data types: It specifies the type and kind of value it can hold.
         // The different types of primitive datatypes are:
@@ -26,11 +27,13 @@ public class Datatypes {
         long longExample = 123456789L;
         System.out.println("Long Example: " + longExample);
 
-        // 5. Float: (Stores values for decimal points. Sufficient for storing 6 to 7 decimals)
+        // 5. Float: (Stores values for decimal points. Sufficient for storing 6 to 7
+        // decimals)
         float floatExample = 12.9898f;
         System.out.println("Float Example: " + floatExample);
 
-        // 6. Double: (Stores values for decimal points. Sufficient for storing up to 15 to 16 decimals)
+        // 6. Double: (Stores values for decimal points. Sufficient for storing up to 15
+        // to 16 decimals)
         double doubleExample = 15.333456d;
         System.out.println("Double Example: " + doubleExample);
 
@@ -42,15 +45,15 @@ public class Datatypes {
         char charExample = 'a';
         System.out.println("Char Example: " + charExample);
 
-
-        //Non-Primitive data type: This datatypes refers to objects or array
+        // Non-Primitive data type: This datatypes refers to objects or array
         // 1. String:-Sequences of characters
         String collegeName = "Nepathya college";
         System.out.println(collegeName);
-        //2.Array: Collection of elements with same data types
-        int[] arrayOfNumbers= {1,3,4,5};
+        // 2.Array: Collection of elements with same data types
+        int[] arrayOfNumbers = { 1, 3, 4, 5 };
         System.out.println(Arrays.toString(arrayOfNumbers));
-     //class: Instance of an object
-     //4. Enom: Constant values in object eg: Male,Female
+        // class: Instance of an object
+        // 4. Enom: Constant values in object eg: Male,Female
+
     }
 }
--- KeyWordsExample.java
+++ KeyWordsExample.java
@@ -1,13 +1,16 @@
 public class KeyWordsExample {
     public static void main(String[] args) {
-        //Constant:It is a value that cannot be changed or modifyed.It is denotaded by final keywords
-       final double PI = 3.14;
+        // Constant:It is a value that cannot be changed or modifyed.It is denotaded by
+        // final keywords
+        final double PI = 3.14;
         System.out.println(PI);
-        //Identifires: They are use to identify variables,function,classes.
+        // Identifires: They are use to identify variables,function,classes.
         // int age =13; //age is a identifyer
-        //literals: Literals are the values that are assigh to the variables
-        String name="Alpha"//string literals
-        int number=34; //intergar literals
+        // literals: Literals are the values that are assigh to the variables
+        String name = "Alpha";// string literals
+        System.out.println(name);
+        int number = 34; // intergar literals
+        System.out.println(number);
     }
-    
+
 }
